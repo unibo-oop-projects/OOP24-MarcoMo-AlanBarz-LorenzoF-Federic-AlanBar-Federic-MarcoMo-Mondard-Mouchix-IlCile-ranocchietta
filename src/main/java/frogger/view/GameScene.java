52e00ab8d0211bea8279a961c82c4d3873cb2b0d
Federico Mariani <federico.mariani3@studio.unibo.it>
2025-02-27 17:40:40 +0100

    created the bases of the view

---
+++ src/main/java/frogger/view/GameScene.java
@@ -0,0 +1,22 @@
+package frogger.view;
+
+import java.awt.Dimension;
+
+import javax.swing.JFrame;
+
+public class GameScene {
+    private JFrame frame;
+    
+    public GameScene(ScenePanel panel) {
+        frame = new JFrame("Ranocchietta");
+        frame.setPreferredSize(new Dimension(600, 800));    //TODO: take the dimension from controller
+        this.frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        this.frame.add(panel);
+        panel.requestFocus();
+
+        frame.pack();
+        frame.setVisible(true);
+    }
+
+}
---
+++ src/main/java/frogger/view/ScenePanel.java
@@ -0,0 +1,7 @@
+package frogger.view;
+
+import javax.swing.JPanel;
+
+public class ScenePanel extends JPanel{
+
+}
