99cbfe4622052b014b15bd8437daa2e75c21dd58
Mondardini Marco <marco.mondardini2@studio.unibo.it>
2025-03-12 10:32:23 +0100

    Fixed build.gradle and removed input logic from gameImpl class

--- src/main/java/frogger/model/implementations/GameImpl.java
+++ src/main/java/frogger/model/implementations/GameImpl.java
@@ -5,12 +5,10 @@ import frogger.common.Pair;
 import frogger.common.Position;
 import frogger.model.interfaces.Game;
 
-import java.awt.event.KeyEvent;
-import java.awt.event.KeyListener;
 import java.util.HashSet;
 import java.util.Set;
 
-public class GameImpl implements Game, KeyListener{
+public class GameImpl implements Game{
 
     private PlayerObjectImpl player;
     private Set<MovingObjectImpl> obstacles;
@@ -29,29 +27,6 @@ public class GameImpl implements Game, KeyListener{
     public int getScore() {
         return this.player.getScore();
     }
-
-    @Override
-    public void keyTyped(KeyEvent e) {}
-
-    @Override
-    public void keyPressed(KeyEvent e) {
-        if (e.getKeyCode() == 38){
-            this.player.setLookingUp();
-            this.player.setPos(new Position(this.player.getPos().x(), this.player.getPos().y() + 1));
-        } else if (e.getKeyCode() == 40){
-            this.player.setLookingDown();
-            this.player.setPos(new Position(this.player.getPos().x(), this.player.getPos().y() - 1));
-        } else if (e.getKeyCode() == 39){
-            this.player.setLookingRight();
-            this.player.setPos(new Position(this.player.getPos().x() + 1, this.player.getPos().y()));
-        } else if (e.getKeyCode() == 37){
-            this.player.setLookingLeft();
-            this.player.setPos(new Position(this.player.getPos().x() - 1, this.player.getPos().y()));
-        }
-    }
-
-    @Override
-    public void keyReleased(KeyEvent e) {}
 
     @Override
     public boolean checkCollision() {
