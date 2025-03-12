030a953353b7ee7a16417fbd40e5876104eb722c
Federico Mariani <federico.mariani3@studio.unibo.it>
2025-03-12 10:55:48 +0100

    created the factory for MovingObject

---
+++ src/main/java/frogger/model/interfaces/MovingObjectFactory.java
@@ -0,0 +1,23 @@
+package frogger.model.interfaces;
+
+import frogger.common.Direction;
+import frogger.common.Pair;
+import frogger.common.Position;
+import frogger.model.implementations.MovingObjectImpl;
+
+public interface MovingObjectFactory {
+
+    /**
+     * generic method to create a MovingObject element
+     * @param <X> type of the MovingObject
+     * @param pos
+     * @param dimension
+     * @param speed
+     * @param direction
+     * @param c java.lang.Class to instance the new MovingObject
+     * @return An object of type X that extends MovingObject
+     */
+    <X extends MovingObjectImpl> X createMovingObject(Position pos, Pair dimension,
+            float speed, Direction direction, Class<X> c);
+
+}
