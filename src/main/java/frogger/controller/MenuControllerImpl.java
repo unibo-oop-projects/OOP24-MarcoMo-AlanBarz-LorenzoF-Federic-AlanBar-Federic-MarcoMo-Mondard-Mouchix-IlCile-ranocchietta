e080630c74b5c5b3fa548cb76f4d63751972413a
Lorenzo Ferri <lorenzo.ferri21@studio.unibo.it>
2025-04-24 23:23:05 +0200

    Added background for the menu and made adjustament in MenuControllore

--- src/main/java/frogger/controller/MenuControllerImpl.java
+++ src/main/java/frogger/controller/MenuControllerImpl.java
@@ -6,7 +6,6 @@ import java.awt.event.MouseMotionListener;
 import frogger.common.Constants;
 import frogger.common.GameState;
 import frogger.common.Pair;
-import frogger.common.input.InputControllerImpl;
 import frogger.common.input.MouseInput;
 import frogger.model.implementations.GameImpl;
 import frogger.view.GameScene;
@@ -15,32 +14,32 @@ import frogger.view.MenuPanel;
 public class MenuControllerImpl implements Controller{
 private final int FPS_SET = 120;
 
-    private GameImpl game;
-    private InputControllerImpl inputController;
+    private GameImpl game;  
     private MenuPanel scenePanel;
+
+    @SuppressWarnings("unused")
     private GameScene gameScene;
-    private MouseInput mouseInput = new MouseInput(this);
 
+    private final MouseInput mouseInput = new MouseInput(this);
 
+    @Override
     public void init(GameScene gameScene) {
         game = new GameImpl(new Pair(Constants.PLAYER_WIDTH,Constants.PLAYER_HEIGHT));
         this.gameScene = gameScene;
-
         scenePanel = new MenuPanel();
         scenePanel.setController(this);
         gameScene.setPanel(scenePanel);
-        inputController = new InputControllerImpl();
     }
 
+    @Override
     public void loop(){
-        double timePerFrame = 1000000000.0 / FPS_SET;
+        double timePerFrame = 10000000.0 / FPS_SET;
         long lastFrame = System.nanoTime();
         long now;
 
         while (GameState.state == GameState.MENU){
             now = System.nanoTime(); 
-            this.inputController.processInput(this.game);
-
+            
             if (now - lastFrame >= timePerFrame) {
                 this.game.getMenu().update();
             }
@@ -54,6 +53,7 @@ private final int FPS_SET = 120;
         this.gameScene = gameScene;
     }
 
+    @Override
     public GameImpl getGame() {
         return game;
     }
--- src/main/java/frogger/view/MenuPanel.java
+++ src/main/java/frogger/view/MenuPanel.java
@@ -3,10 +3,12 @@ package frogger.view;
 import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Graphics;
+import java.awt.image.BufferedImage;
 
 import javax.swing.JPanel;
 
 import frogger.common.Constants;
+import frogger.common.LoadSave;
 import frogger.controller.MenuControllerImpl;
 
 public class MenuPanel extends JPanel{
@@ -34,6 +36,10 @@ public class MenuPanel extends JPanel{
 
     @Override
     public void paintComponent(final Graphics g) {
+        BufferedImage background = LoadSave.GetSprite(LoadSave.MENU_BACKGROUND);
+        g.drawImage(background, (Constants.FRAME_WIDTH/2 -  (background.getWidth()+30)/2), (Constants.FRAME_HEIGHT/2 -  (background.getHeight()+30)/2), 
+                        background.getWidth()+30, background.getHeight()+30, null);
+
         this.controller.getGame().getMenu().draw(g);
     }
 
