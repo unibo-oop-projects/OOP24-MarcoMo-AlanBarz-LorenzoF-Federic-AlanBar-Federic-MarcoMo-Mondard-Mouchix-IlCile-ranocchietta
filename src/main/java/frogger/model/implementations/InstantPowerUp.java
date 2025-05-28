1ff1af55f0e706f96cafbb534a3238a0c61c5b4a
Lorenzo Ferri <lorenzo.ferri21@studio.unibo.it>
2025-05-28 10:13:13 +0200

    Implement PowerUp interface in InstantPowerUp class

--- src/main/java/frogger/model/implementations/InstantPowerUp.java
+++ src/main/java/frogger/model/implementations/InstantPowerUp.java
@@ -2,8 +2,9 @@ package frogger.model.implementations;
 
 import frogger.common.Pair;
 import frogger.common.Position;
+import frogger.model.interfaces.PowerUp;
 
-public abstract class InstantPowerUp extends PickableObjectImpl {
+public abstract class InstantPowerUp extends PickableObjectImpl implements PowerUp {
 
     public InstantPowerUp(Position pos, Pair dimension) {
         super(pos, dimension);
