6604e5bfcd139c3003bf8abcdd514979e417f462
Federico Mariani <federico.mariani3@studio.unibo.it>
2025-05-28 12:34:31 +0200

    fixed some Tests

--- src/test/java/frogger/TrunkTest.java
+++ src/test/java/frogger/TrunkTest.java
@@ -14,7 +14,6 @@ import frogger.common.Pair;
 import frogger.common.Position;
 import frogger.model.implementations.PlayerObjectImpl;
 import frogger.model.implementations.Trunk;
-import frogger.model.interfaces.PlayerObject;
 
 public class TrunkTest {
     private Trunk trunk;
@@ -24,23 +23,23 @@ public class TrunkTest {
         float speed = Constants.MIN_SPEED;
         Direction dir = Direction.LEFT;
         Pair dim = new Pair(Constants.EAGLE_WIDTH, Constants.EAGLE_HEIGHT);
-        Position pos = new Position(10, 10);
+        Position pos = new Position(0, 0);
 
         trunk = new Trunk(pos, dim, speed, dir);
     }
 
     @Test
     void setObjTest() {
-        PlayerObjectImpl frog = new PlayerObjectImpl(new Pair(50,50), null);
-        Position pos = frog.getPos();
+        PlayerObjectImpl frog = new PlayerObjectImpl(new Pair(50,50), "fede.png");
+        frog.setPos(trunk.getPos());
+        Position pos = new Position(frog.getPos().x(), frog.getPos().y());
         trunk.setObj(frog);
         trunk.move();
         assertNotEquals(pos, frog.getPos()); // the object that has been set must move 
         pos = frog.getPos();
         trunk.removeObj();
         trunk.move();
-        assertEquals(pos, frog.getPos()); // Should not move after removing the frog    
-
+        assertEquals(pos, frog.getPos()); // Should not move after removing the frog
     }
 
     @Test
