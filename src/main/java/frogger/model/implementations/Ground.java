21ff640c333ec3b6bb9133ff6e9da34e3ddd7c64
Alan Barzanti - alan.barzanti@studio.unibo.it <alan.barzanti@studio.unibo.it>
2025-06-03 10:40:05 +0200

    Made Ground extend AbstractLaneImpl

--- src/main/java/frogger/model/implementations/Ground.java
+++ src/main/java/frogger/model/implementations/Ground.java
@@ -1,41 +1,27 @@
 package frogger.model.implementations;
 
-import java.util.HashSet;
-import java.util.Set;
-
 import frogger.common.Direction;
-import frogger.model.interfaces.Lane;
 import frogger.model.interfaces.MovingObject;
 
 /**
  * Implements the interface Lane, model the start, mid and last lane of the level.
  */
-public final class Ground implements Lane {
+public final class Ground extends AbstractLaneImpl {
+
+    private Ground(float speed, Direction direction) {
+        super(speed, direction);
+    }
+
+    public Ground() {
+        this(0, null);
+    }
 
     @Override
     public void addMovingObject(final MovingObject obstacle) { }
 
     @Override
-    public float getSpeed() {
-        return 0;
-    }
-
-    @Override
-    public Direction getDirection() {
-        return null;
-    }
-
-    @Override
-    public Set<MovingObject> getLaneObstacles() {
-        return new HashSet<>();
-    }
-
-    @Override
     public boolean isCompleted() {
         return true;
     }
-
-    @Override
-    public void setCompleted() { }
 
 }
