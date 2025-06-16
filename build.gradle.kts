5f0e722ba5d0061b97c256001cc8dd8748e2482d
ilCile <alan.barzanti@studio.unibo.it>
2025-06-16 17:03:48 +0200

    restore build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,7 +12,7 @@ plugins {
      * The runnable jar will be found in build/libs/projectname-all.jar
      */
     id("com.github.johnrengelman.shadow") version "8.1.1"
-    id("org.danilopianini.gradle-java-qa") version "1.119.0"
+    id("org.danilopianini.gradle-java-qa") version "1.96.0"
 }
 
 repositories { // Where to search for dependencies
@@ -21,11 +21,12 @@ repositories { // Where to search for dependencies
 
 dependencies {
     // Suppressions for SpotBugs
-    compileOnly("com.github.spotbugs:spotbugs-annotations:4.9.1")
+    compileOnly("com.github.spotbugs:spotbugs-annotations:4.9.3")
 
     // Maven dependencies are composed by a group name, a name and a version, separated by colons
     implementation("com.omertron:API-OMDB:1.5")
     implementation("org.jooq:jool:0.9.15")
+
     /*
      * Simple Logging Facade for Java (SLF4J) with Apache Log4j
      * See: http://www.slf4j.org/
@@ -33,7 +34,7 @@ dependencies {
     val slf4jVersion = "2.0.17"
     implementation("org.slf4j:slf4j-api:$slf4jVersion")
     // Logback backend for SLF4J
-    runtimeOnly("ch.qos.logback:logback-classic:1.5.17")
+    runtimeOnly("ch.qos.logback:logback-classic:1.5.18")
 
     // JUnit API and testing engine
     val jUnitVersion = "5.11.4"
